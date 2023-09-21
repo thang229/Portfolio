e813cbd63d62bdc5c7c4fcc1495f3fc61f25f790
thang229 <hongthang2292000.com>
2023-09-21 16:26:15 +0700

    Portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+//menu
+const menu = document.querySelector(".navbar_links")
+const menuButton = document.querySelector(".navbar_icons")
+
+menuButton.addEventListener('click', ()=>{
+    menu.classList.toggle("navbar_open");
+    menuButton.classList.toggle("open");
+
+})
